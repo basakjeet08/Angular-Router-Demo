640081dc1a9ef2dc51724c28d40b8a8f9feb5556
Anirban Basak <jeetbasak2002@gmail.com>
2025-02-23 03:58:51 +0530

    feat - created the UI For the Users list route.

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {}
+export class UserListComponent {
+  // Dummy Data to be shown in the UI For now
+  userList = [
+    {
+      name: 'Anirban Basak 01',
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsa repellat praesentium perferendis magni perspiciatis, laudantium earum dolorem deserunt dicta error quis, accusamus atque enim ex molestiae, quas minima voluptates.`,
+    },
+    {
+      name: 'Anirban Basak 02',
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsa repellat praesentium perferendis magni perspiciatis, laudantium earum dolorem deserunt dicta error quis, accusamus atque enim ex molestiae, quas minima voluptates.`,
+    },
+    {
+      name: 'Anirban Basak 03',
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsa repellat praesentium perferendis magni perspiciatis, laudantium earum dolorem deserunt dicta error quis, accusamus atque enim ex molestiae, quas minima voluptates.`,
+    },
+  ];
+
+  // This function is invoked when the user wants to create a new user
+  onCreateUserClick() {
+    alert('Create User Clicked');
+  }
+
+  // This function is invoked when the user wants to delete an user
+  onDeleteUserClick(index: number) {
+    alert(`Delete user on Index ${index} is clicked !!`);
+  }
+}
