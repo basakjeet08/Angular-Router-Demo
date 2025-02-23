67280d55ced2ba7b6f6e1d0ab89e61230a55f35d
Anirban Basak <jeetbasak2002@gmail.com>
2025-02-23 06:24:22 +0530

    feat - User Add feature implemented.

--- src/app/components/user/create-user/create-user.component.ts
+++ src/app/components/user/create-user/create-user.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css'],
 })
-export class CreateUserComponent {}
+export class CreateUserComponent {
+  // Binding to the template references for later use
+  @ViewChild('nameInput') nameInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('descriptionInput')
+  descriptionInput!: ElementRef<HTMLInputElement>;
+
+  // Injecting the user service , routers and current active route
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  // This function is invoked when the user clicks on the create user button
+  onCreateClick() {
+    // Name and description
+    const name = this.nameInput.nativeElement.value.trim();
+    const description = this.descriptionInput.nativeElement.value.trim();
+
+    // Checking if the fields are empty
+    if (!name || !description) {
+      alert('Please Enter all the Fields !!');
+      return;
+    }
+
+    // Sending a create user request to the service
+    this.userService.createUser(name, description);
+
+    // Navigating back to the user list page
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+}
